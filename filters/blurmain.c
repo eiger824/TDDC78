1f7d53e6a89a7f93334fb63fdccba5a58ceba4b6
Santi Pagola <santipagola@gmail.com>
2018-03-26 15:31:40 +0200

    Indent fixes & mpi finalize call

--- filters/blurmain.c
+++ filters/blurmain.c
@@ -8,26 +8,25 @@
 #include "blurfilter.h"
 #include "gaussw.h"
 
+#define MAX_RAD 1000
 
 int main (int argc, char ** argv) {
-   int radius;
+    int radius;
     int xsize, ysize, colmax;
     pixel src[MAX_PIXELS];
     struct timespec stime, etime;
-#define MAX_RAD 1000
-
     double w[MAX_RAD];
 
     /* Take care of the arguments */
 
     if (argc != 4) {
-	fprintf(stderr, "Usage: %s radius infile outfile\n", argv[0]);
-	exit(1);
+        fprintf(stderr, "Usage: %s radius infile outfile\n", argv[0]);
+        exit(1);
     }
     radius = atoi(argv[1]);
     if((radius > MAX_RAD) || (radius < 1)) {
-	fprintf(stderr, "Radius (%d) must be greater than zero and less then %d\n", radius, MAX_RAD);
-	exit(1);
+        fprintf(stderr, "Radius (%d) must be greater than zero and less then %d\n", radius, MAX_RAD);
+        exit(1);
     }
 
     int nprocs;
@@ -44,8 +43,8 @@ int main (int argc, char ** argv) {
         exit(1);
 
     if (colmax > 255) {
-	fprintf(stderr, "Too large maximum color-component value\n");
-	exit(1);
+        fprintf(stderr, "Too large maximum color-component value\n");
+        exit(1);
     }
 
     printf("Has read the image, generating coefficients\n");
@@ -62,14 +61,16 @@ int main (int argc, char ** argv) {
     clock_gettime(CLOCK_REALTIME, &etime);
 
     printf("Filtering took: %g secs\n", (etime.tv_sec  - stime.tv_sec) +
-	   1e-9*(etime.tv_nsec  - stime.tv_nsec)) ;
+            1e-9*(etime.tv_nsec  - stime.tv_nsec)) ;
 
     /* write result */
     printf("Writing output file\n");
-    
-    if(write_ppm (argv[3], xsize, ysize, (char *)src) != 0)
-      exit(1);
 
+    if(write_ppm (argv[3], xsize, ysize, (char *)src) != 0)
+        exit(1);
+
+    /* Finalize MPI running environment */
+    MPI_Finalize();
 
     return(0);
 }

d8deb67eed1c3e0b2d26ff160e85f028293aebd2
Ferb9182 <37694534+Ferb9182@users.noreply.github.com>
2018-05-08 11:41:04 +0200

    Update laplsolv-parallel.f90

--- lab3/laplsolv-parallel.f90
+++ lab3/laplsolv-parallel.f90
@@ -20,7 +20,7 @@ program laplsolv
     integer,dimension(0:99)                   :: start_pos,stop_pos ! Initialize with a big number
     character(len=20)                         :: str
     double precision,dimension(n)             :: tmp2
-    double precision,dimension(0:n-1,0:99)    :: tmp1
+    double precision,dimension(0:n-1,0:99)    :: tmp1,tmp3
 
     ! Set boundary conditions and initial values for the unknowns
     T=0.0D0
@@ -32,62 +32,60 @@ program laplsolv
     nr_threads    = OMP_GET_NUM_THREADS()
     !$omp end parallel
 
-      ! Determine how many stuffs should be implemented by a thread
-  ratio = n/nr_threads
 
-  ! Determine every thread's start/stop position
-  do i=0,nr_threads-1
-      if (i .NE. nr_threads-1) then
-         start_pos(i) = (i*ratio)+1
-         stop_pos(i) = (i+1)*ratio
-      else
-         start_pos(i) = (i*ratio)+1
-         stop_pos(i) = n
-      end if
-  end do
+    ! Determine how many stuffs should be implemented by a thread
+    ratio = n/nr_threads
 
-  ! Solve the linear system of equations using the Jacobi method
-  t0 = omp_get_wtime()
+    ! Determine every thread's start/stop position
+    do i=0,nr_threads-1
+       if (i .NE. nr_threads-1) then
+          start_pos(i) = (i*ratio)+1
+          stop_pos(i) = (i+1)*ratio
+       else
+          start_pos(i) = (i*ratio)+1
+          stop_pos(i) = n
+       end if
+    end do
+
+    ! Solve the linear system of equations using the Jacobi method
+    t0 = omp_get_wtime()
   
-  do k=1,maxiter
+    do k=1,maxiter
      
-     !tmp1=T(1:n,0)
-     error=0.0D0
+       !tmp1=T(1:n,0)
+       error=0.0D0
      
 
-     ! We need to assign various start_pos/stop_pos's previous one array for each thread, their relationship is tmp1=T(1:n,j-1) when tmp2=T(1:n,j)
-     ! tmp3 is an array that consist original stop_pos's value
-     do i=0,nr_threads-1
-         if (i .NE. nr_threads-1) then
-            tmp1(0:n-1, i) = T(1:n, start_pos(i)-1)
-            tmp3(0:n-1, i) = T(1:n, stop_pos(i)+1)
-         else
-            tmp1(0:n-1, i-1) = T(1:n,start_pos(i-1)-1)
-            tmp3(0:n-1, i-1) = T(1:n,stop_pos(i-1)+1)
-         end if
-     end do
+       ! We need to assign various start_pos/stop_pos's previous one array for each thread, their relationship is tmp1=T(1:n,j-1) when tmp2=T(1:n,j)
+       ! tmp3 is an array that consist original stop_pos's value
+       do i=0,nr_threads-1
+          if (i .NE. nr_threads-1) then
+             tmp1(0:n-1, i) = T(1:n, start_pos(i)-1)
+             tmp3(0:n-1, i) = T(1:n, stop_pos(i)+1)
+          else
+             tmp1(0:n-1, i) = T(1:n,start_pos(i)-1)
+             tmp3(0:n-1, i) = T(1:n,stop_pos(i)+1)
+          end if
+       end do
 
-     !$omp parallel private(j,tmp2,my_id) shared(T,tmp1, tmp3)
-     my_id = OMP_GET_THREAD_NUM()
+       !$omp parallel private(j,tmp2,my_id) shared(T,tmp1, tmp3)
+       my_id = OMP_GET_THREAD_NUM()
 
-     do j=start_pos(my_id),(stop_pos(my_id)-1)
-        tmp2=T(1:n,j)
-        T(1:n,j)=(T(0:n-1,j)+T(2:n+1,j)+T(1:n,j+1)+tmp1(0:n-1,my_id))/4.0D0
-        error=max(error,maxval(abs(tmp2-T(1:n,j))))
-        tmp1(0:n-1,my_id)=tmp2
-     end do
-     
-     tmp2=T(1:n,stop_pos(my_id))
-        T(1:n,j)=(T(0:n-1,j)+T(2:n+1,j)+tmp3(0:n-1,my_id)+tmp1(0:n-1,my_id))/4.0D0
-        error=max(error,maxval(abs(tmp2-T(1:n,j))))
-        tmp1(0:n-1,my_id)=tmp2
-     
-     !$omp end parallel
-
-     if (error<tol) then
-        exit
-     end if
-     
+       do j=start_pos(my_id),(stop_pos(my_id)-1)
+          tmp2=T(1:n,j)
+          T(1:n,j)=(T(0:n-1,j)+T(2:n+1,j)+T(1:n,j+1)+tmp1(0:n-1,my_id))/4.0D0
+          error=max(error,maxval(abs(tmp2-T(1:n,j))))
+          tmp1(0:n-1,my_id)=tmp2
+       end do
+       tmp2=T(1:n,stop_pos(my_id))
+       T(1:n,j)=(T(0:n-1,j)+T(2:n+1,j)+tmp3(0:n-1,my_id)+tmp1(0:n-1,my_id))/4.0D0
+       error=max(error,maxval(abs(tmp2-T(1:n,j))))
+       tmp1(0:n-1,my_id)=tmp2
+       !$omp end parallel
+       
+       if (error<tol) then
+          exit
+       end if
   end do
   
   t1 = omp_get_wtime()
